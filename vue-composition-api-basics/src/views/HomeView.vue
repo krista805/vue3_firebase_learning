ab587a97d4faffd6d39dd4262ee3347dbcb85cbb
Krista Simmons <kristasimmons.design@gmail.com>
2022-11-08 08:17:27 -0800

    Computed property example

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -3,10 +3,14 @@
     <h3>{{counterData.title}}</h3>
     <h5>{{ appSubTitle }}</h5>
     <div>
-      <button @click="decreaseCounter" class="btn">-</button>
+      <button @click="decreaseCounter(2)" class="btn">--</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
       <span class="counter">{{counterData.count}}</span>
-      <button @click="increaseCounter" class="btn">+</button>
+      <button @click="increaseCounter(1, $event)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">++</button>
     </div>
+
+    <p>This counter is {{ oddOrEven }}</p>
 
     <div class="edit">
       <h4>Edit counter title:</h4>
@@ -17,7 +21,7 @@
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import { reactive, computed } from 'vue'
 
 // Reactive object
 const counterData = reactive({
@@ -25,16 +29,34 @@ const counterData = reactive({
     title: 'My counter'
 })
 
+//computed property example
+const oddOrEven = computed(() => {
+  if (counterData.count % 2 === 0) return 'even'
+  return 'odd'
+})
+
 // Non reactive way to render data. Sometimes better to do vs reactive for performance reasons
-const appSubTitle = 'An amazing sub title' 
+const appSubTitle = 'An amazing sub title'
 
-const increaseCounter = () => {
-  counterData.count++
+//examle of event being pulled in ('e')
+const increaseCounter = (amount, e) => {
+  counterData.count += amount
 };
 
-const decreaseCounter = () => {
-  counterData.count--
+const decreaseCounter = amount => {
+  counterData.count -= amount
 };
+</script>
+
+<script>
+export default {
+  computed: {
+    myComputedProperty() {
+      //perform logic based on data property
+      return 'my result'
+    }
+  }
+}
 </script>
 
 <style>
