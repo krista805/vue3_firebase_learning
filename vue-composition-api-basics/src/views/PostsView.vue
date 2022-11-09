ff061fc6812bee3583d2c6d31bf2d3567af45462
Krista <ksimmons@solidprofessor.com>
2022-11-09 09:57:15 -0800

    v-for examples

--- vue-composition-api-basics/src/views/PostsView.vue
+++ vue-composition-api-basics/src/views/PostsView.vue
@@ -2,14 +2,11 @@
   <div class="posts">
     <h1>Posts</h1>
     <ul>
-      <li>
-        <RouterLink to="/postDetail/id1">Post 1</RouterLink>
-      </li>
-       <li>
-        <RouterLink to="/postDetail/id2">Post 2</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/postDetail/id3">Post 3</RouterLink>
+      <li 
+        v-for="post in posts"
+        :key="post.id"
+      >
+        <RouterLink :to="`/postDetail/${ post.id }`">{{ post.title }}</RouterLink>
       </li>
     </ul>
 
@@ -25,7 +22,25 @@
   imports
 */
 import { vAutofocus } from '@/directives/vAutofocus'
+import { ref } from 'vue'
 
+/*
+  posts
+*/
+const posts = ref([
+  {
+    id: '1',
+    title: 'Post 1'
+  },
+  {
+    id: '2',
+    title: 'Post 2'
+  },
+  {
+    id: '3',
+    title: 'Post 3'
+  },
+])
 
 
 </script>
