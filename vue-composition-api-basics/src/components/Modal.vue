e095de6627923600e6968e384d127301c6959c0f
Krista Simmons <kristasimmons.design@gmail.com>
2022-11-18 15:59:51 -0800

    Child component example

---
+++ vue-composition-api-basics/src/components/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+        <teleport to='body'>
+            <div class="modal">
+                <h1>This is are modal</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis vel nam, nemo odio temporibus ea, enim
+                    numquam rerum expedita, voluptatem quidem pariatur? Doloremque dolore aperiam voluptatem aut totam non quae!
+                </p>
+                <button>Hide Modal</button>
+            </div>
+        </teleport>
+</template>
+
+<style>
+.modal {
+    background: beige;
+    padding: 10px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+}
+</style>
--- vue-composition-api-basics/src/views/ModalsView.vue
+++ vue-composition-api-basics/src/views/ModalsView.vue
@@ -2,16 +2,7 @@
     <div class="modals">
         <h1>Modals</h1>
         <button @click="showModal = true">Show Modal</button>
-        <teleport to='body'>
-            <div 
-                v-if="showModal"
-                class="modal"
-            >
-                <h1>This is are modal</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis vel nam, nemo odio temporibus ea, enim numquam rerum expedita, voluptatem quidem pariatur? Doloremque dolore aperiam voluptatem aut totam non quae!</p>
-                <button @click="showModal = false">Hide Modal</button>
-            </div>
-        </teleport>
+        <Modal v-if="showModal"></Modal>
     </div>
 </template>
 
@@ -20,6 +11,7 @@
     imports
 */
 import { ref } from 'vue'
+import Modal from '@/components/Modal.vue'
 
 /*
     modals
@@ -29,16 +21,3 @@ const showModal = ref(false)
 ;
 
 </script>
-
-<style>
-    .modal {
-        background: beige;
-        padding:10px;
-        position:  absolute;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height:100%;
-        z-index: 1;
-    }
-</style>
